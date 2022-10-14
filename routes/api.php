ba08213c2d82b25c57be7c375fa47c4f18c03a14
tdarmawan <tdarmawan@hacktiv8.com>
2022-10-14 13:41:54 +0700

    test

--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,4 @@ Route::delete('/customer/{id}', [CustomerController::class, 'destroy']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+ 
