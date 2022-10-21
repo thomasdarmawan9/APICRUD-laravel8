795e5dcc03a1b65c3741f01643f6dfa97dd6bdcf
tdarmawan <tdarmawan@hacktiv8.com>
2022-10-21 10:22:39 +0700

    deploy laravel

--- config/database.php
+++ config/database.php
@@ -45,12 +45,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('MYSQL_URL'),
-            'host' => env('MYSQLHOST', '127.0.0.1'),
-            'port' => env('MYSQLPORT', '3306'),
-            'database' => env('MYSQLDATABASE', 'forge'),
-            'username' => env('MYSQLUSER', 'forge'),
-            'password' => env('MYSQLPASSWORD', ''),
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
